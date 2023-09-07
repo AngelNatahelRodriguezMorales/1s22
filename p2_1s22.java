e0989a669f457577c340a6fa69df013ee0f6ab5e
AngelNatahelRodriguezMorales <144191044+AngelNatahelRodriguezMorales@users.noreply.github.com>
2023-09-06 19:43:03 -0600

    Add files via upload

---
+++ p2_1s22.java
@@ -0,0 +1,9 @@
+class p2_1s22 {
+	public static void main(String[] args) {
+		System.out.println("Angel Natahel");
+		System.out.println("17 años");
+		System.out.println("natahel150805@gmail.com");
+		System.out.println("futbol");
+		System.out.println("azul");
+	}
+}
